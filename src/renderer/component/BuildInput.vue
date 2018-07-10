1e456c29afc18ff9068569a5b13b64d00b835cd5
hfx <jssmith883@gmail.com>
2018-07-11 00:08:05 +0800

    测试了输出日志

--- src/renderer/component/BuildInput.vue
+++ src/renderer/component/BuildInput.vue
@@ -1,10 +1,15 @@
 <template>
     <card dis-hover :bordered="false">
         <strong>TODO</strong> 暂时只是一些说明，计划要把代码准备工作自动化（通过 Git）
+        <i-button type="ghost" @click="runBat">Hello</i-button>
+        <pre>{{outputs.join('\n')}}</pre>
     </card>
 </template>
 
 <script>
+    import {spawn} from 'child_process'
+    import iconv from 'iconv-lite'
+
     export default {
         name: "build-input",
         props: {
@@ -12,10 +17,29 @@
                 type: Object,
                 required: true,
                 default() {
-                    return {}
+                    return {
+                    }
                 }
             }
         },
+        data() {
+            return {
+                outputs: []
+            }
+        },
+        computed: {
+        },
+        methods: {
+            runBat() {
+                let ls = spawn('cmd.exe', ['/c', 'C:\\Users\\hfx\\Desktop\\Build_Platform\\task_run.bat'])
+
+                ls.stdout.on('data', data => {
+                    data = iconv.decode(new Buffer(data), 'gbk', {stripBOM: false})
+                    this.outputs.push(data)
+                });
+
+            }
+        }
     }
 </script>
 
